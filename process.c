d5eaeda815838594e517d83cedfe20e251e12d93
Abhijeet Saha <abhijeetayon@gmail.com>
2020-12-23 19:20:43 -0500

    fixed some stuff

--- process.c
+++ process.c
@@ -9,7 +9,7 @@
 int phi(int n){ 
     int ans = n; 
     for (int p = 2; p * p <= n; ++p) { 
-        if (n % p == 0) { 
+        if (n % p == 0) {
             while (n % p == 0) 
                 n /= p; 
             ans -= ans / p; 
@@ -23,23 +23,25 @@ int phi(int n){
 int main(){
     char input [256];
     char output [256];
-    int fd1 = open("./mario", O_RDONLY);
-    int fd2 = open("./luigi", O_WRONLY);
-    int fd3 = open("hi.txt", O_WRONLY);
+    int pipe;
     
     while(1){
         // read number from mario
-        read(fd1, input, 256);
+        pipe = open("mario", O_RDONLY);
+        read(pipe, input, 256);
+        close(pipe);
         // find phi(n)
         int a = atoi(input);
         int ans = phi(a);
-        // convert to char array
+        // write answer to char array
+        
         sprintf(output, "%d", ans); 
+
         // write to luigi so prompt.c can read it
-        write(fd2, output, sizeof(output));
-        write(fd3, output, sizeof(output));
+        open("luigi", O_WRONLY);
+        write(pipe, input, sizeof(input));
+        printf("output: %s written to luigi\n", output);
+        close(pipe);
     }
-    close(fd1);
-    close(fd2);
     return 0;
 }
--- prompt.c
+++ prompt.c
@@ -1,33 +1,33 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(){
     char input [256];
     char output [256];
-    int inpipe = mkfifo("./mario", 0644);
-    int outpipe = mkfifo("./luigi", 0664);
-    int fd1 = open("./mario", O_WRONLY);
-    int fd2 = open("./luigi", O_RDONLY);
+    mkfifo("mario", 0666);
+    mkfifo("luigi", 0666);
+    int pipe;
     while(1){
         // recieve input
-        printf("Please enter a number: ");
+        printf("Please enter a positive integer: ");
         fgets(input, 256, stdin);
-
+        *strchr(input, '\n') = 0;
+        pipe = open("mario", O_WRONLY);
         // write input to mario to be read by process.c
         
-        write(fd1, input, sizeof(input));
-
+        write(pipe, input, sizeof(input));
+        close(pipe);
         // recieve output from process.c that was written to luigi and print it
-        
-        read(fd2, output, sizeof(output));
-
+        pipe = open("luigi", O_RDONLY);
+        read(pipe, output, sizeof(output));
         printf("phi(n): %s\n", output);
+        close (pipe);
     }
-    close(fd1);
-    close(fd2);
     return 0;
 }
